c807c26e96a15056f369c468487f79e5f7e3963a
Melina Blanco <melinablanco88@gmail.com>
2023-07-04 16:33:12 -0600

    finish site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,25 +6,25 @@ export const Footer = () => {
 	return (
 		<footer id='footer' className="black-bg text-center">
 			<Container>
-				<Row className="p-5">
+				<Row className="pt-5 pb-5">
 					<Image
 						className='logo-image'
 						src='./images/logo-heyroomie-white.png'
 						alt='Logo Hey! Roomie'
 					/>
 					<h1 className="title-section">Follow Us</h1>
-					<Stack direction='horizontal' gap={2} className='social-nav d-flex justify-content-center'>
+					<Stack direction='horizontal' gap={1} className='social-nav d-flex justify-content-center'>
 						<a href="https://twitter.com/HeyRoomieApp" className='p-3 mx-2 hvr-radial-out' target="_blank" rel="noreferrer">
-							<BsTwitter size={32} />
+							<BsTwitter />
 						</a>
 						<a href="https://www.facebook.com/heyroomieapp" className='p-3 mx-2 hvr-radial-out' target="_blank" rel="noreferrer">
-							<BiLogoFacebook size={32} />
+							<BiLogoFacebook />
 						</a>
 						<a href="https://www.instagram.com/heyroomieapp/" className='p-3 mx-2 hvr-radial-out' target="_blank" rel="noreferrer">
-							<BsInstagram size={32} />
+							<BsInstagram />
 						</a>
 						<a href="https://www.linkedin.com/company/hey-roomie" className='p-3 mx-2 hvr-radial-out' target="_blank" rel="noreferrer">
-							<BiLogoLinkedin size={32} />
+							<BiLogoLinkedin />
 						</a>
 					</Stack>
 				</Row>
