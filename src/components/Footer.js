fc7083031a7d8090a1c0d7dab93ff51d98ca5fff
Melina Blanco <melinablanco88@gmail.com>
2023-07-04 12:58:41 -0600

    checking

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
-import { Image, Stack, Row, Container } from "react-bootstrap";
-import { AiFillFacebook, AiOutlineInstagram, AiFillYoutube } from "react-icons/ai";
-import { FaTwitter } from "react-icons/fa";
+import { Image, Row, Container } from "react-bootstrap";
 
 export const Footer = () => {
 	return (
@@ -14,21 +12,6 @@ export const Footer = () => {
 						width='122px'
 						style={{ objectFit: "contain" }}
 					/>
-
-					<Stack direction='horizontal' gap={1} className='d-flex justify-content-center pt-2 pt-md-0'>
-						<a href="https://twitter.com/FarmaLeal/" className='p-0 text-white mx-2'>
-							<FaTwitter size={22} />
-						</a>
-						<a href="https://www.facebook.com/CFarmaLeal/" className='p-0 text-white mx-2'>
-							<AiFillFacebook size={22} />
-						</a>
-						<a href="https://www.instagram.com/farmaleal/" className='p-0 text-white mx-2'>
-							<AiOutlineInstagram size={22} />
-						</a>
-						<a href="https://www.youtube.com/channel/UCVezBeVMt6iPAsq2a9_4CIA" className='p-0 text-white mx-2'>
-							<AiFillYoutube size={22} />
-						</a>
-					</Stack>
 				</Row>
 			</Container>
 		</Footer>
