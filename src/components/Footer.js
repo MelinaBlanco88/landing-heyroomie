b9b81b6a8817bf20c7f0e149ecae7503a93f82bb
Melina Blanco <melinablanco88@gmail.com>
2023-07-04 13:29:44 -0600

    with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import { Image, Stack, Row, Container } from "react-bootstrap";
-import { AiFillFacebook, AiOutlineInstagram, AiFillYoutube } from "react-icons/ai";
+import { TiSocialTwitterCircular, TiSocialFacebookCircular, TiSocialInstagramCircular, TiSocialLinkedinCircular } from "react-icons/ti";
 import { FaTwitter } from "react-icons/fa";
 
 export const Footer = () => {
 	return (
 		<footer id='footer' className="black-bg text-center">
 			<Container>
-				<Row>
+				<Row className="p-5">
 					<Image
 						className='logo-image'
 						src='./images/logo-heyroomie-white.png'
@@ -14,17 +14,17 @@ export const Footer = () => {
 					/>
 					<h1 className="title-section">Follow Us</h1>
 					<Stack direction='horizontal' gap={1} className='d-flex justify-content-center pt-2 pt-md-0'>
-						<a href="https://twitter.com/FarmaLeal/" className='p-0 text-white mx-2'>
-							<FaTwitter size={22} />
+						<a href="https://twitter.com/HeyRoomieApp" className='p-0 text-white mx-2'>
+							<TiSocialTwitterCircular size={72} />
 						</a>
-						<a href="https://www.facebook.com/CFarmaLeal/" className='p-0 text-white mx-2'>
-							<AiFillFacebook size={22} />
+						<a href="https://www.facebook.com/heyroomieapp" className='p-0 text-white mx-2'>
+							<TiSocialFacebookCircular size={22} />
 						</a>
-						<a href="https://www.instagram.com/farmaleal/" className='p-0 text-white mx-2'>
-							<AiOutlineInstagram size={22} />
+						<a href="https://www.instagram.com/heyroomieapp/" className='p-0 text-white mx-2'>
+							<TiSocialInstagramCircular size={22} />
 						</a>
-						<a href="https://www.youtube.com/channel/UCVezBeVMt6iPAsq2a9_4CIA" className='p-0 text-white mx-2'>
-							<AiFillYoutube size={22} />
+						<a href="https://www.linkedin.com/company/hey-roomie" className='p-0 text-white mx-2'>
+							<TiSocialLinkedinCircular size={22} />
 						</a>
 					</Stack>
 				</Row>
