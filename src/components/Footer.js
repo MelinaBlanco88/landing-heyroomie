b16c0397587a9188c4d90e8b828f87d5b72690ee
Melina Blanco <melinablanco88@gmail.com>
2023-07-04 13:19:07 -0600

    with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,34 @@
-import { Image, Row, Container } from "react-bootstrap";
+import { Image, Stack, Row, Container } from "react-bootstrap";
+import { AiFillFacebook, AiOutlineInstagram, AiFillYoutube } from "react-icons/ai";
+import { FaTwitter } from "react-icons/fa";
 
 export const Footer = () => {
 	return (
-		<Footer id='footer' className="black-bg">
+		<footer id='footer' className="black-bg text-center">
 			<Container>
 				<Row>
 					<Image
+						className='logo-image'
 						src='./images/logo-heyroomie-white.png'
 						alt='Logo Hey! Roomie'
-						height='auto'
-						width='122px'
-						style={{ objectFit: "contain" }}
 					/>
+					<h1 className="title-section">Follow Us</h1>
+					<Stack direction='horizontal' gap={1} className='d-flex justify-content-center pt-2 pt-md-0'>
+						<a href="https://twitter.com/FarmaLeal/" className='p-0 text-white mx-2'>
+							<FaTwitter size={22} />
+						</a>
+						<a href="https://www.facebook.com/CFarmaLeal/" className='p-0 text-white mx-2'>
+							<AiFillFacebook size={22} />
+						</a>
+						<a href="https://www.instagram.com/farmaleal/" className='p-0 text-white mx-2'>
+							<AiOutlineInstagram size={22} />
+						</a>
+						<a href="https://www.youtube.com/channel/UCVezBeVMt6iPAsq2a9_4CIA" className='p-0 text-white mx-2'>
+							<AiFillYoutube size={22} />
+						</a>
+					</Stack>
 				</Row>
 			</Container>
-		</Footer>
+		</footer>
 	);
 };
