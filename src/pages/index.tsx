17b36f42d578a8bce089f3dc04eda0d7826dd866
Melina Blanco <melinablanco88@gmail.com>
2023-08-10 14:58:46 -0600

    add adsense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ export default function Home() {
         <title>Hey! Roomie</title>
         <meta name="description" content="Hey! Roomie is a cutting-edge platform that aims to transform the way people find roommates and shared living spaces." />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3144810430632001" crossOrigin="anonymous"></script>
       </Head>
       <Header />
       <Body />
